cfb282b7b60a8a69f7f45b660dfb8b9ae3119a2d
Yassine Idrissi <yaidriss@c2r4p8.1337.ma>
2024-03-04 22:35:58 +0100

    clear the debug print in the ex01 really

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 22:52:43 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/03/04 22:19:32 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/03/04 22:34:05 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,29 +49,9 @@ int operation(RPN_t& r)
         nm1 = r.nm[r.nm.size() -2] * r.nm[r.nm.size() -1];
     else if (r.op[0] == '/')
         nm1 = r.nm[r.nm.size() -2] / r.nm[r.nm.size() -1];
-    std::cout << "the first :" << r.nm[r.nm.size() -2] << std::endl; 
-    std::cout << "the second :" << r.nm[r.nm.size() -1] << std::endl;
     r.op.pop_back();
     r.nm.pop_back();
     r.nm.pop_back();
     r.nm.push_back(nm1);
-    std::cout << YELLOW << "After : the first :" << r.nm[r.nm.size() -1] << RESET << std::endl; 
     return 0;
-    // std::cout << "AFTER the second :" << r.nm[r.nm.size() -2] << RESET << std::endl;
-    // std::cout << "the first :" << r.nm[0] << std::endl;
-    // std::cout << "the second :" << r.nm[1] << std::endl;
-    // std::cout << "the op :" << r.op[0] << std::endl;
 }
-
-// int calculNumber(RPN& r)
-// {
-    
-// }
-
-// int check_v(RPN& r)
-// {
-//     for (std::deque<int>::iterator it = r.c.begin(); it != r.c.end(); ++it)
-//         if (it->size() != 1 || (!std::isdigit((*it)[r.nm.size() -1]) && (*it)[r.nm.size() -1] != '-' && (*it)[r.nm.size() -1] != '*' && (*it)[r.nm.size() -1] != '+' && (*it)[r.nm.size() -1] != '/'))
-//             return calculNumber(r);
-//     return 1;
-// }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 22:50:01 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/03/04 22:21:32 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/03/04 22:34:27 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,18 @@ int parsing(char *av, RPN& r)
 {
 	int check = 0;
 	r.ln = split(av, ' ');
-	std::cout << "the line size" << r.ln.size() << std::endl;
 	for(size_t i =0 ; i < r.ln.size() && !check; i++)
 	{
 		if(r.ln[i].size() == 1 && (r.ln[i][0] == '-' || r.ln[i][0] == '*' || r.ln[i][0] == '+' || r.ln[i][0] == '/'))
 		{
-			std::cout << "first op " << r.ln[i] << std::endl;
 			r.op.push_back(r.ln[i][0]);
 			check = operation(r);
 		}
 		else if (r.ln[i].size() == 1 && std::isdigit(r.ln[i][0]))
-		{
-			std::cout << "SECOND op" << r.ln[i] << std::endl;
 			r.nm.push_back(r.ln[i][0] - '0');
-		}
 		else
 			return(printErreur(2));
 	}
-	// if(r.op.size() < 1 || r.op.size() != r.nm.size() - 1)
-	// 	return(printErreur(2));
 	return check;
 }
 
@@ -45,6 +38,5 @@ int main(int ac, char **av)
 		if(parsing(av[1], r))
 			return 1;
 	std::cout << GREEN << r.nm[0] << RESET << std::endl;
-	// std::cerr << RED << "Error" << RESET << std::endl;
 	return 0;
 }
