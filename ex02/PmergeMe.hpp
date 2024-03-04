bc365f2f6764ce2b96951ee2a09f8e42d1ccfb8c
Yassine Idrissi <yaidriss@c2r4p8.1337.ma>
2024-03-04 22:35:36 +0100

    clear the debug print in the ex01

---
+++ ex02/PmergeMe.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 13:29:46 by yaidriss          #+#    #+#             */
+/*   Updated: 2024/03/03 13:32:05 by yaidriss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include <string>
+
+typedef struct pm_t
+{
+	
+}pm;
+
+//********* Colors *****************//
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+
+//************* print Functions ***********//
+
+
+
+//************ Functions ******************//
