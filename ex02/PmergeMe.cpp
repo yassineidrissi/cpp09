0b0e05ff293be9b1de4cd92b8af5170332e8386c
Yassine Idrissi <yaidriss@c2r4p8.1337.ma>
2024-03-05 01:47:47 +0100

    create ex02 and print the output

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,9 +6,18 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:29:52 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/03/03 13:34:30 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/03/05 00:01:26 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
+std::vector<std::string> split(const std::string &s, char delimiter) 
+{
+	std::vector<std::string> lines;
+	std::string ln;
+	std::istringstream tokenStream(s);
+	while (std::getline(tokenStream, ln, delimiter)) 
+		lines.push_back(ln);
+	return lines;
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 13:34:36 by yaidriss          #+#    #+#             */
-/*   Updated: 2024/03/03 15:37:23 by yaidriss         ###   ########.fr       */
+/*   Updated: 2024/03/05 01:46:06 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int main(int ac, char **av)
 {
-	
+	if (ac == 1)
+	{
+		std::cerr << RED << "Error: No arguments" << RESET << std::endl;
+		return 1;
+	}
+	else
+		std::cout << "Arguments: " << av[1] << std::endl;
 }
